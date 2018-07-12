fde3ac15ff47e62547e5d408a201db71debdbda6
Daniel Diaz <daniel@sirius.utp.edu.co>
2018-07-12 10:28:11 -0500

    Added helmet server request

--- src/server/main.js
+++ src/server/main.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 import colors from "colors/safe";
 import morgan from "morgan";
+import helmet from "helmet";
 
 // Create app
 const app = express();
@@ -9,10 +10,14 @@ console.warn(colors.blue(`Environment: ${ app.get("env") }`));
 
 // Serve files
 app.use(express.static(path.resolve(__dirname, "../../dist")));
+
 // Logger
 if (app.get("env") === "test" || app.get("env") === "development") {
   app.use(morgan("dev"));
 }
+
+// Helmet
+app.use(helmet());
 
 // Use Frontend Routes
 require("./routes/index.js")(app, "/*");
